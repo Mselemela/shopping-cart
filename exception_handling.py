b3539701fa140a5ff63c29f1ee3ee51528a968f2
Mpho <mselemela10@gmail.com>
2025-07-03 07:37:08 +0200

    name error

--- exception_handling.py
+++ exception_handling.py
@@ -1,4 +1,4 @@
-
+'''
 try:
     print(x)
     
@@ -10,3 +10,19 @@ except:
     
 finally:
     print("The 'try except' is finished")
+  '''  
+    
+try:
+    print(x)
+    
+except NameError:
+    print("Variable x is not defined")
+    
+else:
+    print("Everything went wrong")
+    
+    
+x = -1
+
+if x < 0:
+    raise Exception("Sorry, no numbers below zero")
