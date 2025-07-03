e3f9dfe1f72a402a82387722496661732ce74b88
Mpho <mselemela10@gmail.com>
2025-07-03 07:29:08 +0200

    exception handling

---
+++ exception_handling.py
@@ -0,0 +1,12 @@
+
+try:
+    print(x)
+    
+# except NameError:
+#     print("Variable x is not defined")
+
+except:
+    print("an eception occurred")
+    
+finally:
+    print("The 'try except' is finished")
